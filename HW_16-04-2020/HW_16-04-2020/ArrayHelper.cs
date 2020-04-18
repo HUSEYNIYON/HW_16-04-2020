d3b6389a6e6bfd939a99b6797c26227d9ad8275b
Huseyn <your>
2020-04-18 23:01:09 +0300

    Build a Pop command

---
+++ HW_16-04-2020/HW_16-04-2020/ArrayHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_16_04_2020
+{
+    class ArrayHelper <T>
+    {
+        public T Pop(ref T[] Array)
+        {
+
+            T[] arr = new T[Array.Length - 1];
+            for (int i = 0; i < Array.Length - 1; i++)
+            {
+                arr[i] = Array[i];
+
+            }
+            T a = Array[Array.Length - 1];
+            Array = arr;
+            return a;
+        }
+    }
+}
---
+++ HW_16-04-2020/HW_16-04-2020/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_16_04_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a size of an array: ");
+            int arrSize = Convert.ToInt32(Console.ReadLine());
+            int[] digits =new int[arrSize];
+            Random rand = new Random();
+            Console.WriteLine("Before:");
+            for(int i=0;i<digits.Length;i++)
+            {
+                digits[i] = rand.Next(9);
+            }
+            foreach(int digit in digits)
+            {
+                Console.Write(" "+digit+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("After:");
+            ArrayHelper<int> ar = new ArrayHelper<int>();
+            ar.Pop(ref digits);
+            foreach (int digit in digits)
+            {
+                Console.Write(" "+digit+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+
+   
+}
